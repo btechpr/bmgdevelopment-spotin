0801a13d2cd39c905fa4e01a1f023ba10ae00f67
gabrielnieves18 <gabrielnieves18@gmail.com>
2016-07-09 21:10:46 -0400

    Added a new UnitTest class

---
+++ app/src/test/java/com/bmgdevelopment/android/spotin/ExampleUnitTest2.java
@@ -0,0 +1,24 @@
+package com.bmgdevelopment.android.spotin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Created by gabrielnieves18 on 07-09-16.
+ */
+public class ExampleUnitTest2 {
+
+    @Test
+    public void myname_isCorrect() throws Exception {
+        assertArrayEquals("Gabriel".toCharArray(), "Gabriel".toCharArray());
+    }
+
+    @Test
+    public void myname_isNotCorrect() throws Exception {
+        assertArrayEquals("Gabriel".toCharArray(), "GabrieL".toCharArray());
+    }
+
+}
